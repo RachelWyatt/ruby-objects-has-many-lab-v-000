8befa0c685d3285cf93ab5ee2e8714b27a9a7d38
Rachel Wyatt <rw126@evansville.edu>
2018-09-24 21:19:04 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,14 +10,14 @@ class Author
     @posts
   end
 
-  def add_post(post)
-    @post << post
+  def add_post(name)
+    @post << name
     post.author = self
   end
 
-  def add_post_by_title(post)
+  def add_post_by_title(name)
     post = Post.new(name)
-    @post << post
+    @post << name
     post.author = self
   end
 
