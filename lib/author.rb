a8dcd5f3773e8acecff7108294539598d76f5036
Rachel Wyatt <rw126@evansville.edu>
2018-09-24 20:47:12 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,18 @@
+class Author
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_post(post)
+  end
+
+  def add_post_by_title(post)
+  end
+
+  def post_count
+  end
+
+end
