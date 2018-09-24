15cae93baba681d19abdc80cf02416dea45c2c75
Rachel Wyatt <rw126@evansville.edu>
2018-09-24 20:54:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,20 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << name
+    @artist = artist
   end
 
   def self.all
     @@all
+  end
+
+  def artist
+    @artist
   end
 
 end
